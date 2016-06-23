9eed4e4020b9bd6cadf871d4d8703f78d4fbc391
WanionCane <wanioncane@gmail.com>
2016-06-22 23:02:37 -0300

    fixed an english typo, also, created pt_BR.lang.

--- src/main/java/wanion/thermsingul/ThermalSingularityItem.java
+++ src/main/java/wanion/thermsingul/ThermalSingularityItem.java
@@ -16,7 +16,7 @@ public final class ThermalSingularityItem extends ItemSingularity
 
     public static final int[] recipeValues = new int[]{75, 25, 100, 90, 75};
     public static final String[] thermTypes = new String[]{"shiny", "manaInfused", "signalum", "lumium", "enderium"};
-    private static final int[] lighter = new int[]{0x69D6FC, 0x6499BC, 0xE55D00, 0xD8B44E, 0x107272};
+    private static final int[] clearer = new int[]{0x69D6FC, 0x6499BC, 0xE55D00, 0xD8B44E, 0x107272};
     private static final int[] darker = new int[]{0x44CCFC, 0x436071, 0xA52300, 0xD4A41B, 0x093A3F};
 
     private ThermalSingularityItem()
@@ -28,7 +28,7 @@ public final class ThermalSingularityItem extends ItemSingularity
     @Override
     public int getColorFromItemStack(ItemStack itemstack, int renderPass)
     {
-        return renderPass == 0 ? darker[getDamage(itemstack)] : lighter[getDamage(itemstack)];
+        return renderPass == 0 ? darker[getDamage(itemstack)] : clearer[getDamage(itemstack)];
     }
 
     @Override
